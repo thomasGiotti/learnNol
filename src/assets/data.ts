473c4fcb1cf981ce372e6383f497da0acf72cf5c
thomasGiotti <tho.giotti@gmail.com>
2023-07-01 15:26:43 +0200

    add punctuation

--- src/assets/data.ts
+++ src/assets/data.ts
@@ -15,7 +15,7 @@ export const services = [
     id: 3,
     title: "Maintenance",
     content:
-      "Contrôles réguliers, mises à jour logicielles, anticipation des pannes possibles ou de problèmes techniques de votre parc informatique",
+      "Contrôles réguliers, mises à jour logicielles, anticipation des pannes possibles ou de problèmes techniques de votre parc informatique.",
   },
   {
     id: 4,
@@ -33,7 +33,7 @@ export const services = [
     id: 6,
     title: "Gaming",
     content:
-      "Création et de maintenance de serveurs Counter-Strike. Configuration initiale du serveur selon vos besoins spécifiques, en incluant les paramètres de jeu, les plugins, les modèles de carte, et plus encore",
+      "Création et de maintenance de serveurs Counter-Strike. Configuration initiale du serveur selon vos besoins spécifiques, en incluant les paramètres de jeu, les plugins, les modèles de carte, et plus encore.",
   },
 ];
 
@@ -50,6 +50,6 @@ export const crypto = [
     title: "Vente de RIGs clef en main",
     content:
       " Des rigs de minage clés en main, prêts à l'emploi, pour vous permettre de vous lancer facilement dans l'activité de minage de crypto-monnaies. Nos rigs sont soigneusement assemblés avec des composants de haute qualité, optimisés pour offrir des performances maximales en matière de minage. Nous prenons en charge l'installation du système d'exploitation, des pilotes et des logiciels de minage, afin que vous puissiez commencer à miner dès que vous recevez votre rig. De plus, notre équipe est disponible pour vous fournir un support technique continu et des conseils pour optimiser vos rendements. Simplifiez votre expérience de minage avec nos rigs clés en main.",
-    more: "Formation au minage de crypto-monnaies incluse lors de l'achat de nos RIGs",
+    more: "Formation au minage de crypto-monnaies incluse lors de l'achat de nos RIGs.",
   },
 ];
